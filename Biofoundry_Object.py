5f99dba2d6c427025535a96ae2e6922425524e7d
Yvonnefoo <31839833+Yvonnefoo@users.noreply.github.com>
2018-01-02 12:03:57 +0800

    Update Biofoundry_Object.py

--- Biofoundry_Object.py
+++ Biofoundry_Object.py
@@ -201,20 +201,20 @@ for PCR_reactions in PCR_reactions_list:
 
 plot ([TotalTimeList])
 
-#plate splice
-a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96] 
-b = [97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192]
-c = [193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255, 256, 257, 258, 259, 260, 261, 262, 263, 264, 265, 266, 267, 268, 269, 270, 271, 272, 273, 274, 275, 276, 277, 278, 279, 280, 281, 282, 283, 284, 285, 286, 287, 288]
-d = [289, 290, 291, 292, 293, 294, 295, 296, 297, 298, 299, 300, 301, 302, 303, 304, 305, 306, 307, 308, 309, 310, 311, 312, 313,314, 315, 316, 317, 318, 319, 320, 321, 322, 323, 324, 325, 326, 327, 328, 329, 330, 331, 332, 333, 334, 335, 336, 337, 338, 339, 340, 341, 342, 343, 344, 345, 346, 347, 348, 349, 350, 351, 352, 353, 354, 355, 356, 357, 358, 359, 360, 361, 362, 363, 364, 365, 366, 367, 368, 369, 370, 371, 372, 373, 374, 375, 376, 377, 378, 379, 380, 381, 382, 383, 384]
-
 #Dr Foo's Protocols
-MasterMixPlate = Plate('MasterMixPlate',6,1,5000,5000)
-PCRPlate = Plate('PCRPlate',384,0,50,0)
+MasterMixPlate = Plate('MasterMixPlate',6,1,10000,10000)
+PCRPlateA = Plate('PCRPlateA',96,0,50,0)
+PCRPlateB = Plate('PCRPlateB',96,0,50,0)
+PCRPlateC = Plate('PCRPlateC',96,0,50,0)
+PCRPlateD = Plate('PCRPlateD',96,0,50,0)
 PrimerPlate1 = Plate('PrimerPlate1',96,96,360,100)
 PrimerPlate2 = Plate('PrimerPlate2',96,96,360,100)
 PrimerPlate3 = Plate('PrimerPlate3',96,96,360,100)
 PrimerPlate4 = Plate('PrimerPlate4',96,96,360,100)
-ECHOPlate = Plate('ECHOPlate', 384,0,65,0)
+ECHOPlateA = Plate('ECHOPlateA', 96,0,65,0)
+ECHOPlateB = Plate('ECHOPlateB', 96,0,65,0)
+ECHOPlateC = Plate('ECHOPlateC', 96,0,65,0)
+ECHOPlateD = Plate('ECHOPlateD', 96,0,65,0)
 PCRPlate1 = Plate('PCRPlate1',96,0,360,0)
 PCRPlate2 = Plate('PCRPlate2',96,0,360,0)
 PCRPlate3 = Plate('PCRPlate3',96,0,360,0)
@@ -224,25 +224,31 @@ Echo = Liquid_Handler2(1)
 Thermalcycler = Thermal_Cycler()
 
 #Distribution of Master Mix
-Opentrons.transfer(MasterMixPlate,PCRPlate,10)
+Opentrons.transfer(MasterMixPlate,PCRPlateA,10)
+Opentrons.transfer(MasterMixPlate,PCRPlateB,10)
+Opentrons.transfer(MasterMixPlate,PCRPlateC,10)
+Opentrons.transfer(MasterMixPlate,PCRPlateD,10)
 
 #Transferring Primers to ECHO plate
-Opentrons.transfer(PrimerPlate1,ECHOPlate.wells(a),10)
-Opentrons.transfer(PrimerPlate2,ECHOPlate.wells(b),10)
-Opentrons.transfer(PrimerPlate3,ECHOPlate.wells(c),10)
-Opentrons.transfer(PrimerPlate4,ECHOPlate.wells(d),10)
+Opentrons.transfer(PrimerPlate1,ECHOPlateA,10)
+Opentrons.transfer(PrimerPlate2,ECHOPlateB,10)
+Opentrons.transfer(PrimerPlate3,ECHOPlateC,10)
+Opentrons.transfer(PrimerPlate4,ECHOPlateD,10)
 
 #Transfer of primers from ECHO plate to PCR plate
-Echo.transfer(ECHOPlate,PCRPlate,2.5)
+Echo.transfer(ECHOPlateA,PCRPlateA,2)
+Echo.transfer(ECHOPlateB,PCRPlateB,2)
+Echo.transfer(ECHOPlateC,PCRPlateC,2)
+Echo.transfer(ECHOPlateD,PCRPlateD,2)
 
 #Transfer of PCR mixture to respective plates
-Opentrons.transfer(PCRPlate.wells(a),PCRPlate1,10)
-Opentrons.transfer(PCRPlate.wells(b),PCRPlate2,10)
-Opentrons.transfer(PCRPlate.wells(c),PCRPlate3,10)
-Opentrons.transfer(PCRPlate.wells(d),PCRPlate4,10)
+Opentrons.transfer(PCRPlateA,PCRPlate1,10)
+Opentrons.transfer(PCRPlateB,PCRPlate2,10)
+Opentrons.transfer(PCRPlateC,PCRPlate3,10)
+Opentrons.transfer(PCRPlateD,PCRPlate4,10)
 
 #Performing PCR reactions for each plate
-Thermalcycler.PCR(PCRPlate1)
-Thermalcycler.PCR(PCRPlate2)
-Thermalcycler.PCR(PCRPlate3)
-Thermalcycler.PCR(PCRPlate4)
+Thermalcycler.PCR(PCRPlate1, 60)
+Thermalcycler.PCR(PCRPlate2, 60)
+Thermalcycler.PCR(PCRPlate3, 60)
+Thermalcycler.PCR(PCRPlate4, 60)
